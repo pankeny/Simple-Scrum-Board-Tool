8039c6647780a4d88d316bdd980e17bf7b3a7555
pankeny <konrad_malocha@hotmail.com>
2019-04-29 22:06:19 +0200

    code refactor MapValidationError as self-contained Service

---
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/services/MapValidationErrorService.java
@@ -0,0 +1,28 @@
+package io.github.pankeny.ssbtapi.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> MapValidationService(BindingResult result){
+
+        if(result.hasErrors()) {
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            result.getFieldErrors().forEach( err -> errorMap.put(err.getField(), err.getDefaultMessage()));
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        return null;
+    }
+
+}
--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/web/ProjectController.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/web/ProjectController.java
@@ -1,22 +1,18 @@
 package io.github.pankeny.ssbtapi.web;
 
 import io.github.pankeny.ssbtapi.domain.Project;
+import io.github.pankeny.ssbtapi.services.MapValidationErrorService;
 import io.github.pankeny.ssbtapi.services.ProjectService;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -25,20 +21,18 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()) {
-
-            Map<String, String> errorMap = new HashMap<>();
-
-            result.getFieldErrors().forEach( err -> errorMap.put(err.getField(), err.getDefaultMessage()));
-
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
 
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
+
     }
 
 }
