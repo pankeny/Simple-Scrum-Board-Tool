01a758d7ac9db136663cb03e8aa769acb5c29231
pankeny <konrad_malocha@hotmail.com>
2019-04-29 21:44:07 +0200

    add error handling when input data is not valid

--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/web/ProjectController.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/web/ProjectController.java
@@ -2,16 +2,21 @@ package io.github.pankeny.ssbtapi.web;
 
 import io.github.pankeny.ssbtapi.domain.Project;
 import io.github.pankeny.ssbtapi.services.ProjectService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,11 +29,16 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
         if(result.hasErrors()) {
-            return new ResponseEntity<>("Invalid project object", HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            result.getFieldErrors().forEach( err -> errorMap.put(err.getField(), err.getDefaultMessage()));
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
-        Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 
 }
