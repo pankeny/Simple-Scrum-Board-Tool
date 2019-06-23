ffc617bd5dc307733b532285bf99126057d6bf70
pankeny <konrad_malocha@hotmail.com>
2019-06-23 12:03:37 +0200

    ssbt-client-003 create basic layout

--- ssbt-client/src/App.js
+++ ssbt-client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
+      <Footer />
     </div>
   );
 }
