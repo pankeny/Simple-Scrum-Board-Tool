8cc3e8c12a4ca447048cdb127ce0ebb5881ddd62
pankeny <konrad_malocha@hotmail.com>
2019-06-26 21:39:38 +0200

    ssbt-client-006 dashboard without project item

--- ssbt-client/src/components/Dashboard.js
+++ ssbt-client/src/components/Dashboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import ProjectItem from "./Project/ProjectItem";
+import { Button } from "reactstrap";
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="projects">
+      <div className="container-fluid mt-3">
         <h2>Projects</h2>
+        <div className="new-project-section mx-5">
+          <Button className="new-project-button ">
+            {" "}
+            <span>New Project</span>
+          </Button>
+          <hr />
+        </div>
+
+        <div className="container project-item m-4">
+          <p>test</p>
+        </div>
       </div>
     );
   }
