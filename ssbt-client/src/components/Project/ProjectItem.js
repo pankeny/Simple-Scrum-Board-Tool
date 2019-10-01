c87d8d7e3190a74de1fa73bf57dfcf9ead20a4ff
pankeny <konrad_malocha@hotmail.com>
2019-10-01 19:49:32 +0200

    ssbt-07 delete project handling

--- ssbt-client/src/components/Project/ProjectItem.js
+++ ssbt-client/src/components/Project/ProjectItem.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
-import { Button, Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import avatar from "../../assets/yoda.png";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { deleteProject } from "../../actions/projectActions";
 
 class ProjectItem extends Component {
+  onDeleteClick = id => {
+    this.props.deleteProject(id);
+  };
+
   render() {
     const { project } = this.props;
     return (
@@ -19,20 +26,24 @@ class ProjectItem extends Component {
           </Col>
           <Col lg={{ size: 3, offset: 1 }} className="project-buttons">
             {" "}
-            <Button className="project-button d-block my-2 ml-auto">
-              {" "}
-              <span>Project Board</span>
-            </Button>{" "}
+            <Link
+              to={`/projectBoard/${project.projectIdentifier}`}
+              className="btn btn-md project-button d-block my-2 ml-auto"
+            >
+              Project Board
+            </Link>{" "}
             <Link
               to={`/updateProject/${project.projectIdentifier}`}
               className="btn btn-md project-button d-block my-2 ml-auto"
             >
               Update Project
             </Link>{" "}
-            <Button className="project-button d-block my-2 ml-auto">
-              {" "}
-              <span>Delete Project</span>
-            </Button>
+            <Link
+              onClick={this.onDeleteClick.bind(this, project.projectIdentifier)}
+              className="btn btn-md project-button d-block my-2 ml-auto"
+            >
+              Delete Project
+            </Link>{" "}
           </Col>
         </Row>
       </div>
@@ -40,4 +51,11 @@ class ProjectItem extends Component {
   }
 }
 
-export default ProjectItem;
+ProjectItem.propTypes = {
+  deleteProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteProject }
+)(ProjectItem);
