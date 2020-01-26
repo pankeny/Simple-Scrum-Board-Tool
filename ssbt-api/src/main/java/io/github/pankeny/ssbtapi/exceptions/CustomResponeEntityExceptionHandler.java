bcef36ac788c73a8964c3b3477ded1ab41168bc2
pankeny <konrad_malocha@hotmail.com>
2020-01-26 19:07:53 +0100

    ssbt-13 add project not found exception

--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/exceptions/CustomResponeEntityExceptionHandler.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/exceptions/CustomResponeEntityExceptionHandler.java
@@ -17,4 +17,10 @@ public class CustomResponeEntityExceptionHandler extends ResponseEntityException
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> projectNotFoundException(ProjectNotFoundException ex, WebRequest request){
+        ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/services/ProjectTaskService.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/services/ProjectTaskService.java
@@ -1,11 +1,16 @@
 package io.github.pankeny.ssbtapi.services;
 
 import io.github.pankeny.ssbtapi.domain.Backlog;
+import io.github.pankeny.ssbtapi.domain.Project;
 import io.github.pankeny.ssbtapi.domain.ProjectTask;
+import io.github.pankeny.ssbtapi.exceptions.ProjectNotFoundException;
 import io.github.pankeny.ssbtapi.repositories.BacklogRepository;
+import io.github.pankeny.ssbtapi.repositories.ProjectRepository;
 import io.github.pankeny.ssbtapi.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProjectTaskService {
@@ -17,29 +22,44 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
 
-        projectTask.setBacklog(backlog);
-        Integer backlogSequence = backlog.getPTSequence();
-        ++backlogSequence;
-        backlog.setPTSequence(backlogSequence);
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
 
-        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer backlogSequence = backlog.getPTSequence();
+            ++backlogSequence;
+            backlog.setPTSequence(backlogSequence);
 
-        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
-            projectTask.setPriority(3);
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
+
+            if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(3);
+            }
+
+            if (projectTask.getStatus() == null || projectTask.getStatus().isEmpty()) {
+                projectTask.setStatus("TO-DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception exception) {
+            throw new ProjectNotFoundException("Project not Found");
         }
 
-        if (projectTask.getStatus() == null || projectTask.getStatus().isEmpty()) {
-            projectTask.setStatus("TO-DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+        Optional<Project> project = Optional.ofNullable(projectRepository.findByProjectIdentifier(id));
+
+        if(!project.isPresent()) {
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
