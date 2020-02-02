d3fb520b2bf100ffa204c1fb55631a25e8682a4e
pankeny <konrad_malocha@hotmail.com>
2020-02-02 22:17:17 +0100

    style project board

--- ssbt-client/src/components/ProjectBoard/ProjectBoard.js
+++ ssbt-client/src/components/ProjectBoard/ProjectBoard.js
@@ -22,18 +22,18 @@ class ProjectBoard extends Component {
                             {
                                 //!-- SAMPLE PROJECT TASK STARTS HERE --
                             }
-                            <div className="card mb-1 bg-light">
+                            <div className="card mb-1 bg-dark">
 
-                                <div className="card-header text-primary">
+                                <div className="card-header button-text">
                                     ID: projectSequence -- Priority: priorityString
                                 </div>
-                                <div className="card-body bg-light">
+                                <div className="card-body bg-dark">
                                     <h5 className="card-title">project_task.summary</h5>
                                     <p className="card-text text-truncate ">
                                         project_task.acceptanceCriteria
                                     </p>
-                                    <a href="#" className="btn btn-primary">
-                                        View / Update
+                                    <a href="#" className="btn project-button">
+                                        Update
                                     </a>
 
                                     <button className="btn btn-danger ml-4">
@@ -46,9 +46,26 @@ class ProjectBoard extends Component {
                             }
                         </div>
                         <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-primary text-white">
-                                    <h3>In Progress</h3>
+                            <div className="col-header my-2">
+                                <p>In Progress</p>
+                            </div>
+                            <div className="card mb-1 bg-dark">
+
+                                <div className="card-header button-text">
+                                    ID: projectSequence -- Priority: priorityString
+                                </div>
+                                <div className="card-body bg-dark">
+                                    <h5 className="card-title">project_task.summary</h5>
+                                    <p className="card-text text-truncate ">
+                                        project_task.acceptanceCriteria
+                                    </p>
+                                    <a href="#" className="btn project-button">
+                                        Update
+                                    </a>
+
+                                    <button className="btn btn-danger ml-4">
+                                        Delete
+                                    </button>
                                 </div>
                             </div>
                             {
@@ -59,9 +76,26 @@ class ProjectBoard extends Component {
 
                         </div>
                         <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-success text-white">
-                                    <h3>Done</h3>
+                            <div className="col-header my-2">
+                                <p>Done</p>
+                            </div>
+                            <div className="card mb-1 bg-dark">
+
+                                <div className="card-header button-text">
+                                    ID: projectSequence -- Priority: priorityString
+                                </div>
+                                <div className="card-body bg-dark">
+                                    <h5 className="card-title">project_task.summary</h5>
+                                    <p className="card-text text-truncate ">
+                                        project_task.acceptanceCriteria
+                                    </p>
+                                    <a href="#" className="btn project-button">
+                                        Update
+                                    </a>
+
+                                    <button className="btn btn-danger ml-4">
+                                        Delete
+                                    </button>
                                 </div>
                             </div>
                             {
