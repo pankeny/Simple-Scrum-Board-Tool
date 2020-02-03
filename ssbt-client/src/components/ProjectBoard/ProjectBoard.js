7ceb068d1d3d5fefff4d3e91d1d6c82bd9e430e6
pankeny <konrad_malocha@hotmail.com>
2020-02-03 05:55:08 +0100

    fix imports

--- ssbt-client/src/components/ProjectBoard/ProjectBoard.js
+++ ssbt-client/src/components/ProjectBoard/ProjectBoard.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
 import AddProjectTaskButton from "./ProjectTasks/AddProjectTaskButton";
 
 class ProjectBoard extends Component {
