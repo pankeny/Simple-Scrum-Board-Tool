136cfd866d439b5e9e0fc5f03cc9bcde18b0970d
pankeny <konrad_malocha@hotmail.com>
2020-02-19 17:05:02 +0100

    assign equivalent class depends on task priority

--- ssbt-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ ssbt-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -3,11 +3,30 @@ import React, {Component} from 'react';
 class ProjectTask extends Component {
     render() {
         const {project_task} = this.props;
+        let priorityString;
+        let priorityClass;
+
+        if (project_task.priority === 1) {
+            priorityClass = "bg-danger";
+            priorityString = "HIGH";
+        }
+
+        if (project_task.priority === 2) {
+            priorityClass = "bg-warning";
+            priorityString = "MEDIUM";
+        }
+
+        if (project_task.priority === 3) {
+            priorityClass = "bg-info";
+            priorityString = "LOW";
+        }
+
         return (
+
+            // Todo - define css class for priority that changes border color of card (not header bg like now)
             <div className="card mb-1 bg-dark">
-                <div className="card-header button-text">
-                    ID: {project_task.projectSequence} -- Priority: {" "}
-                    {project_task.priority}
+                <div className={`card-header ${priorityClass}`}>
+                    ID: {project_task.projectSequence} -- Priority: {priorityString}
                 </div>
                 <div className="card-body bg-dark">
                     <h5 className="card-title">{project_task.summary}</h5>
