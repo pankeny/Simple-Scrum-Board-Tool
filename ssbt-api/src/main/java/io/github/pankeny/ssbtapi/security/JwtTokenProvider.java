0118a294cc9a5b6e989ba275b707e283ecbb666a
pankeny <konrad_malocha@hotmail.com>
2020-03-05 20:04:18 +0100

    ssbt-24 validate and use jwt in requests

--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/security/JwtTokenProvider.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/security/JwtTokenProvider.java
@@ -1,8 +1,9 @@
 package io.github.pankeny.ssbtapi.security;
 
 import io.github.pankeny.ssbtapi.domain.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -17,9 +18,7 @@ import static io.github.pankeny.ssbtapi.security.SecurityConstants.SECRET;
 
 @Component
 public class JwtTokenProvider {
-
-    // Todo >>> generate the token
-
+    Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
     public String generateToken(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
 
@@ -43,7 +42,30 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    // Todo >>> validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException exception) {
+            logger.error("Invalid JWT Signature");
+        } catch (MalformedJwtException exception) {
+            logger.error("Invalid JWT Token");
+        } catch (ExpiredJwtException exception) {
+            logger.error("Expired JWT Token");
+        } catch (UnsupportedJwtException exception) {
+            logger.error("Unsupported JWT Token");
+        } catch (IllegalArgumentException exception) {
+            logger.error("JWT claims string is empty");
+        }
 
-    // Todo >>> get userId from token
+        return false;
+    }
+
+    public Long getUserIdFromJwt(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.parseLong((String) claims.get("id"));
+    }
 }
--- ssbt-api/src/main/java/io/github/pankeny/ssbtapi/security/SecurityConfig.java
+++ ssbt-api/src/main/java/io/github/pankeny/ssbtapi/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static io.github.pankeny.ssbtapi.security.SecurityConstants.H2_URL;
 import static io.github.pankeny.ssbtapi.security.SecurityConstants.SIGN_UP_URLS;
@@ -34,6 +35,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    ;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -79,5 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest()
                 .authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
