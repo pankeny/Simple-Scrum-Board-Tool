697fde6ffaf3c835a0f679ddb1109efea9815776
pankeny <konrad_malocha@hotmail.com>
2020-04-18 18:12:55 +0200

    ssbt-27 implement routes security

--- ssbt-client/src/App.js
+++ ssbt-client/src/App.js
@@ -4,20 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import AddProject from "./components/Project/AddProject";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
-import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
-import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 import Login from "./components/Authentication/Login";
 import Register from "./components/Authentication/Register";
 import jwt_decode from "jwt-decode";
 import setJwtToken from "./securityUtils/setJwtToken";
 import {SET_CURRENT_USER} from "./actions/types";
 import {logout} from "./actions/securityActions";
+import AddProject from "./components/Project/AddProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import SecuredRoute from "./securityUtils/SecuredRoute";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -48,20 +49,26 @@ function App() {
 
                         {
                             // Public routes
+
                         }
+                        <Route exact path="/" component={Login}/>
+                        < Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Register}/>
 
                         {
                             // Private routes
+
                         }
-                        <Route exact path="/" component={Login}/>
-                        <Route exact path="/dashboard" component={Dashboard}/>
-                        <Route exact path="/login" component={Login}/>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/addProject" component={AddProject}/>
-                        <Route exact path="/updateProject/:id" component={UpdateProject}/>
-                        <Route exact path="/projectBoard/:id" component={ProjectBoard}/>
-                        <Route exact path="/addProjectTask/:id" component={AddProjectTask}/>
-                        <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask}/>
+                        <Switch>
+                            <SecuredRoute exact path="/dashboard" component={Dashboard}/>
+                            <SecuredRoute exact path="/addProject" component={AddProject}/>
+                            <SecuredRoute exact path="/updateProject/:id" component={UpdateProject}/>
+                            <SecuredRoute exact path="/projectBoard/:id" component={ProjectBoard}/>
+                            <SecuredRoute exact path="/addProjectTask/:id" component={AddProjectTask}/>
+                            <SecuredRoute exact path="/updateProjectTask/:backlog_id/:pt_id"
+                                          component={UpdateProjectTask}/>
+                        </Switch>
+
                     </div>
                     <Footer/>
                 </div>
